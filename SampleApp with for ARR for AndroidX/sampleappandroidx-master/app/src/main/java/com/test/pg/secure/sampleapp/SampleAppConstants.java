9380b20f77fa1e2c222b2094f4339838b6b935dd
Sivaramyogesh Manimaran <41984137+sivaramyogesh@users.noreply.github.com>
2020-09-01 17:10:59 +0530

    sample app two

---
+++ SampleApp with for ARR for AndroidX/sampleappandroidx-master/app/src/main/java/com/test/pg/secure/sampleapp/SampleAppConstants.java
@@ -0,0 +1,36 @@
+package com.test.pg.secure.sampleapp;
+
+public class SampleAppConstants {
+    //API_KEY is given by the Payment Gateway. Please Copy Paste Here.
+    public static final String PG_API_KEY = "XXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXXXX";
+
+    //URL to Accept Payment Response Afdashboardter Payment. This needs to be done at the client's web server.
+    public static final String PG_RETURN_URL = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+
+    //Enter the Mode of Payment Here . Allowed Values are "LIVE" or "TEST".
+    public static final String PG_MODE = "TEST";
+
+    //PG_CURRENCY is given by the Payment Gateway. Only "INR" Allowed.
+    public static final String PG_CURRENCY = "INR";
+
+    //PG_COUNTRY is given by the Payment Gateway. Only "IND" Allowed.
+    public static final String PG_COUNTRY = "IND";
+
+    public static final String PG_AMOUNT = "2";
+    public static final String PG_EMAIL = "XXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    public static final String PG_NAME = "XXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    public static final String PG_PHONE = "9876543210";
+    public static       String PG_ORDER_ID = "";
+    public static final String PG_DESCRIPTION = "XXXXXXXXXXXXXX";
+    public static final String PG_CITY = "XXXXXXXXXXXXXX";
+    public static final String PG_STATE = "Maharashtra";
+    public static final String PG_ADD_1 = "XXXXXXXXXXXXXX";
+    public static final String PG_ADD_2 = "Mumbai";
+    public static final String PG_ZIPCODE = "401107";
+    public static final String PG_UDF1 = "";
+    public static final String PG_UDF2 = "";
+    public static final String PG_UDF3 = "";
+    public static final String PG_UDF4 = "";
+    public static final String PG_UDF5 = "";
+
+}
